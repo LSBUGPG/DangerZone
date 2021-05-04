7d358441299e5a8065b090e033a5ab4cb7b72c29
Paul Sinnett <paul.sinnett@gmail.com>
2021-05-04 15:16:30 +0100

    Initial commit

---
+++ Assets/Components/DangerZone/Component/DangerZone.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DangerZone : MonoBehaviour
+{
+    public float damagePerSecond = 10.0f;
+    
+    void OnTriggerStay(Collider collider)
+    {
+        collider.SendMessage("TakeDamage", damagePerSecond * Time.deltaTime, SendMessageOptions.DontRequireReceiver);
+    }
+}
---
+++ Assets/Components/DangerZone/Examples/Health.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Slider indicator;
+    public int maxHitPoints;
+    float hitPoints;
+
+    void Start()
+    {
+        hitPoints = maxHitPoints;
+    }
+
+    public void TakeDamage(float damage)
+    {
+        hitPoints -= damage;
+        if (hitPoints < 0.0f)
+        {
+            hitPoints = 0.0f;
+        }
+    }
+
+    void Update()
+    {
+        indicator.value = hitPoints / (float)maxHitPoints;
+    }
+}
